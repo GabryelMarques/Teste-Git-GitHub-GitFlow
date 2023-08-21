726cda4279ab70d5678c22ceb4cc42815e880cdb
Gabryel Marques <gabryelmarques17@gmail.com>
2023-08-21 17:29:18 -0300

    Função para adicionar arestas

--- Matriz_pesos.py
+++ Matriz_pesos.py
@@ -3,3 +3,7 @@ class Grafo:
     def __init__(self, vertices):
         self.vertices = vertices
         self.grafo = [[0]*self.vertices for i in range(self.vertices)]
+
+    def adiciona_aresta(self,u,v,peso ):
+        self.grafo[u-1][v-1] = peso 
+        self.grafo[v-1][u-1] = peso 
